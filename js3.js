43d94fd27d7f133fd6ec283accda23579f50c0ac
T. Virág <virmoc411@gmail.com>
2021-09-15 21:21:23 +0200

    3.feladat javítása

--- js3.js
+++ js3.js
@@ -2,7 +2,7 @@ var versTomb = [
     {
         versCim: "Metró",
         szerzo:"Varró Dániel",
-        vers:"Hát elkapott ma, kiscicám,<br>az ellenőr a metrón.<br>Nem volt érvényes matricám,<br>kívánom, bárha lett vón.<br><br>Leszállított – az alagút<br>most énelőttem ásít,<br>s eljutnom több mint bonyolult<br>a kívánt állomásig.<br><br>S míg kattog egyre távolabb<br>haladva lent a metró,<br>halkan kattog a bőr alatt<br>az ember szíve dettó.<br><br>Ó, bár ne kéne lógnia,<br>föladva minden elvét!<br>Ez itt egy allegória,<br>ha nem tűnt volna fel még.<br><br>Hisz látod, éppen így megyek<br>hogy földerítsem, úgy ám,<br>felszín alatti énedet<br>a lélek mélyvasútján.<br><br>De megvívnám bár érted, ó,<br>én héroszok tusáját,<br>nem jutok el hozzád a szó<br>szűk labirintusán át.<br><br>S a vágy metróján, hol az ok<br>zord ellenőre szétcsap,<br>mint potyautas utazok,<br>azt kell hogy mondjam, én csak.<br><br>Mert nincsen matricám, se más,<br>jegy, bérlet, bármi érvény,<br>a benned rejlő állomást<br>hogy egyszer is elérném.<br><br>S meglelve benned messzi, tág,<br>mély állomások mását,<br>meghallanám a bőrön át<br>a szíved kattogását.",
+        vers:"az alagút<br>most énelőttem ásít,<br>s eljutnom több mint bonyolult<br>a kívánt állomásig.<br><br>S míg kattog egyre távolabb<br>haladva lent a metró,<br>halkan kattog a bőr alatt<br>az ember szíve dettó.<br><br>Ó, bár ne kéne lógnia,<br>föladva minden elvét!<br>Ez itt egy allegória,<br>ha nem tűnt volna fel még.<br><br>Hisz látod, éppen így megyek<br>hogy földerítsem, úgy ám,<br>felszín alatti énedet<br>a lélek mélyvasútján.<br><br>De megvívnám bár érted, ó,<br>én héroszok tusáját,<br>nem jutok el hozzád a szó<br>szűk labirintusán át.<br><br>S a vágy metróján, hol az ok<br>zord ellenőre szétcsap,<br>mint potyautas utazok,<br>azt kell hogy mondjam, én csak.<br><br>Mert nincsen matricám, se más,<br>jegy, bérlet, bármi érvény,<br>a benned rejlő állomást<br>hogy egyszer is elérném.<br><br>S meglelve benned messzi, tág,<br>mély állomások mását,<br>meghallanám a bőrön át<br>a szíved kattogását." ,
         kep:"kepek/kep1.jpg"
     },
     {
@@ -13,14 +13,21 @@ var versTomb = [
     },
 ];
 $(function(){
-    /*object.onclick = kattint(){elso};*/
-    object.addEventListener("click", elso);
-    object.addEventListener("click", masodik);
-});
-$(function kattint(){
-    for (let i = 0; i < versTomb.length; i++) {
-        const element = versTomb[i];
-        $(".elem1").append("<div><h3>"+element["versCim"] + "</h3><img src='" + element.kep+ " ' alt = 'szerző'>" + "<p>element.vers</p>"+"<p>+element.szerzo</p></div>");
-        
+    $("#elso").on("click", elsorekattint);
+    $("#masodik").on("click", masodikrakattint);
     
-} )
+});
+function elsorekattint(){
+    for (let i = 0; i < 1; i++) {
+        const element = versTomb[0];
+        $(".elem1").append("<div><h3>"+element["versCim"] + "</h3>" + "<p>"+[element.vers] + "</p>"+"<p>"+element.szerzo+"</p></div>");
+        
+    }
+}
+function masodikrakattint(){
+    for (let i = 1; i < 2; i++) {
+        const element = versTomb[1];
+        $(".elem2").append("<div><h3>"+element["versCim"] + "</h3>" + "<p>"+ [element.vers] + "</p>"+"<p>"+element.szerzo+"</p></div>");
+        
+    }
+}
